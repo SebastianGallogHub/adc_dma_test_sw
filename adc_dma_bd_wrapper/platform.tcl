8d89b441d8284b018039a4e22f99349bf65ccff5
Sebastián Gallo <1sebastiangallo@gmail.com>
2024-11-19 09:34:11 -0300

    Initial commit

---
+++ adc_dma_bd_wrapper/platform.tcl
@@ -0,0 +1,22 @@
+# 
+# Usage: To re-create this platform project launch xsct with below options.
+# xsct /home/sebas/Escritorio/proyecto/workspace/adc_dma_test/vitis_workspace/adc_dma_bd_wrapper/platform.tcl
+# 
+# OR launch xsct and run below command.
+# source /home/sebas/Escritorio/proyecto/workspace/adc_dma_test/vitis_workspace/adc_dma_bd_wrapper/platform.tcl
+# 
+# To create the platform in a different location, modify the -out option of "platform create" command.
+# -out option specifies the output directory of the platform project.
+
+platform create -name {adc_dma_bd_wrapper}\
+-hw {/home/sebas/Escritorio/proyecto/workspace/adc_dma_test/adc_dma_bd_wrapper.xsa}\
+-out {/home/sebas/Escritorio/proyecto/workspace/adc_dma_test/vitis_workspace}
+
+platform write
+domain create -name {standalone_ps7_cortexa9_0} -display-name {standalone_ps7_cortexa9_0} -os {standalone} -proc {ps7_cortexa9_0} -runtime {cpp} -arch {32-bit} -support-app {empty_application}
+platform generate -domains 
+platform active {adc_dma_bd_wrapper}
+domain active {zynq_fsbl}
+domain active {standalone_ps7_cortexa9_0}
+platform generate -quick
+platform generate
